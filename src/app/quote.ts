5931fb563996775a5720f89adae3df02e97af773
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-08-21 17:48:17 +0300

    created a method to show quotes

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,10 +1,14 @@
 export class Quote {
-    id:number;
-    word:string;
-    author:string;
-    constructor(id:number, word:string, author:string){
-        this.id =id;
-        this.word = word;
-        this.author=author;
+    showQuotes:boolean;
+    constructor(public id:number, public word:string, public author:string){
+        this.showQuotes = false;
     }
+    // id:number;
+    // word:string;
+    // author:string;
+    // constructor(id:number, word:string, author:string){
+    //     this.id =id;
+    //     this.word = word;
+    //     this.author=author;
+    // }
 }

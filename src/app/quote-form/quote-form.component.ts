532b66cc8f362e2085ea5a50e499ff47fd08d405
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-08-22 21:32:17 +0300

    added forms

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,6 @@
+// import { Quote } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-form',
@@ -7,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteFormComponent implements OnInit {
 
+  newQuote = new Quote("","","", new Date(), 0, 0);
   constructor() { }
 
   ngOnInit(): void {
